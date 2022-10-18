aa8cb19c1f0e1bcaebaeca0ec0a95a8c2d66cd9f
@Godspeed78 <miracleoghenemado@gmail.com>
2022-10-18 08:41:01 +0100

    updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e378daac26da1b0c2ccd1be25cdc8577.js"
+  "/precache-manifest.2e9307aad62456e2c499deed7179edc5.js"
 );
 
 self.addEventListener('message', (event) => {
